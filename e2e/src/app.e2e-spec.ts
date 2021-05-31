dd2f3d996edfead3419fa2c277a15b141dacfc14
RafalM2000 <rfm@europe.com>
2021-05-31 11:42:02 +0200

    dodanie 2 testów

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -13,6 +13,16 @@ describe('workspace-project App', () => {
     expect(page.getTitleText()).toEqual('home works!');
   });
 
+  it('should display 3 elements in details view', () => {
+    page.navigateToDetails();
+    expect(page.getItemsList().count()).toBe(3);
+  });
+
+  it('should display "Konstanty, Anna, Kitty"', () => {
+    page.navigateToDetails();
+    expect(page.getItemListText()).toEqual(['Konstanty', 'Anna', 'Kitty']);
+  });
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,8 +6,19 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
+  navigateToDetails(): Promise<unknown> {
+    return browser.get('details') as Promise<unknown>;
+  }
+
   getTitleText(): Promise<unknown> {
     return element(by.css('app-home p')).getText() as Promise<unknown>;
   }
 
+  getItemsList(): ElementArrayFinder {
+    return element.all(by.css('ul li')) as ElementArrayFinder;
+  }
+
+  getItemListText(): Promise<unknown> {
+    return element.all(by.css('ul li')).getText() as Promise<unknown>;
+  }
 }
